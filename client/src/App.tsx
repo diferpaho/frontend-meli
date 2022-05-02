9348550f696b3b907c59298410ea4b5e4feb387d
diferpaho <diferpaho@gmail.com>
2022-05-02 07:36:47 -0500

    add navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="navbar">
+          <div className="container mx-5">
+            <div className='row mx-5 px-5 w-100'>
+              <a className="navbar-brand col-1" href="#">
+                <img src="https://www.expoknews.com/wp-content/uploads/2020/03/1200px-MercadoLibre.svg-1.png" alt="" width="60" height="30" className="d-inline-block align-text-top" />
+              </a>
+              <div className="input-group col">
+                <input type="text" className="form-control" placeholder="Nunca dejes de buscar" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                <button className="input-group-text" type="button" id="button-addon2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+
+          </div>
+        </nav>
       </header>
+
+      <section className='container'>
+
+      </section>
     </div>
   );
 }
